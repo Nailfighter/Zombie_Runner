c85529ec402d9f5c98bd38ada11b71b722d43b3c
Nailfighter <nailfighter000@gmail.com>
2020-10-10 12:14:48 +0530

    Post Processing

--- Assets/Script/Player_Health.cs
+++ Assets/Script/Player_Health.cs
@@ -1,19 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 public class Player_Health : MonoBehaviour
 {
+    public PostProcessVolume m_Volume;
+    public  Vignette m_Vignette;
+
+    [SerializeField] Color color_blood;
     [SerializeField] int Health_P;
     [SerializeField] Slider Health_Slider;
     [SerializeField] GameObject Death_UI;
+    private void Start()
+    {
+        Instantiat_Quick_Volume();
+    }
     public void enemy_hit(int enemy_damage)
     {
         Health_P -= enemy_damage;
+        StartCoroutine(Post_Processsing_Hit());
         Health_Slider.value = Health_P;
         check_if_dead();
     }
+
+    IEnumerator Post_Processsing_Hit()
+    {
+
+        m_Vignette.enabled.Override(true);
+        yield return new WaitForSeconds(1f);
+        m_Vignette.enabled.Override(false);
+    }
+
+    private void Instantiat_Quick_Volume()
+    {
+        m_Vignette = ScriptableObject.CreateInstance<Vignette>();
+        m_Vignette.intensity.Override(0.4f);
+        m_Vignette.color.Override(color_blood);
+        m_Vignette.smoothness.Override(1f);
+        m_Vignette.roundness.Override(1f);
+        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
+    }
+
     void check_if_dead()
     {
         if (Health_P <= 0)
